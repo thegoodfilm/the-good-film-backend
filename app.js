66d639778e54e3f6ecfc13fea37b0b7de21d43eb
sarastackhouse <sarapalacios7@gmail.com>
2020-12-16 09:33:39 +0100

    Connected to front

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3001", "https://thegoodfilm.netlify.app"],
   })
 );
 
